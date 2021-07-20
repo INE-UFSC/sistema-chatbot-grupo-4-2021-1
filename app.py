4cc5b2b2beaf6390f1e63f534fb137bdbf44e782
joaomai <joaomai2001@gmail.com>
2021-07-20 12:01:04 -0300

    novo bot, o sulmatogrossense

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 #encoding: utf-8
-from Bots.BotAgiota import BotAgiota
 from SistemaChatBot import SistemaChatBot as scb
 from Bots.BotCansado import BotCansado
+from Bots.BotAgiota import BotAgiota
+from Bots.BotSulMatogrossense import BotSulMatogrossense
 
 ###construa a lista de bots disponíveis aqui
-lista_bots = [BotCansado("Mai"), BotAgiota("Hulk")]
+lista_bots = [BotCansado("Mai"), BotAgiota("Hulk"), BotSulMatogrossense("Maria")]
 
 sys = scb.SistemaChatBot("Bots Irados", lista_bots)
 sys.inicio()
