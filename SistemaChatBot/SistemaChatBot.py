35efaeb0936df1d1974654e194bd23dd693e233a
Enzo-Bassani <65874230+Enzo-Bassani@users.noreply.github.com>
2021-07-20 11:24:08 -0300

    Update SistemaChatBot.py

--- SistemaChatBot/SistemaChatBot.py
+++ SistemaChatBot/SistemaChatBot.py
@@ -2,36 +2,51 @@ from Bots.Bot import Bot
 
 class SistemaChatBot:
     def __init__(self,nomeEmpresa,lista_bots):
-        self.__empresa=nomeEmpresa
+        self.__empresa = nomeEmpresa
         ##verificar se a lista de bots contém apenas bots
         self.__lista_bots=lista_bots
         self.__bot = None
     
     def boas_vindas(self):
-        pass
-        ##mostra mensagem de boas vindas do sistema
+        print("Olá, seja bem vindo ao nosso bate papo com bots! Só tome cuidado para não esquecer do mundo real.")
 
     def mostra_menu(self):
-        pass
-        ##mostra o menu de escolha de bots
+        print("Nós temos os seguintes BOTs disponíveis:")
+        for i, bot in enumerate(self.__lista_bots):
+            print(i, "-", bot.nome)
     
     def escolhe_bot(self):
-        pass
-        ##faz a entrada de dados do usuário e atribui o objeto ao atributo __bot 
+        while True:
+            ibot = input("Escolha o seu bot pelo número: ")
+            try:
+                ibot = int(ibot)
+            except:
+                print("O valor inserido deve ser um número")
+                continue
+
+            if ibot >= 0 and ibot < len(self.__lista_bots):
+                self.__bot = self.__lista_bots[ibot]
+                break
+            else:
+                print("Não há um bot com esse número")
 
     def mostra_comandos_bot(self):
-        pass
-        ##mostra os comandos disponíveis no bot escolhido
+        print(self.__bot.mostra_comandos())
 
     def le_envia_comando(self):
-        pass
-        ##faz a entrada de dados do usuário e executa o comando no bot ativo
+        cmd = input("Insira o comando, ou insira -1 para sair")
+        if cmd == "-1":
+            return False
+        self.__bot.executa_comando(cmd)
 
     def inicio(self):
-        pass
-        ##mostra mensagem de boas-vindas do sistema
-        ##mostra o menu ao usuário
-        ##escolha do bot      
-        ##mostra mensagens de boas-vindas do bot escolhido
-        ##entra no loop de mostrar comandos do bot e escolher comando do bot até o usuário definir a saída
-        ##ao sair mostrar a mensagem de despedida do bot
+        self.boas_vindas()
+        self.mostra_menu()
+        self.escolhe_bot()
+        self.__bot.boas_vindas()
+        while True:
+            print(self.mostra_comandos_bot)
+            if not self.le_envia_comando:
+                break
+        self.__bot.despedida()
+
