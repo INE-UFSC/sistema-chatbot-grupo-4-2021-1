f965cbb178398056061acc7fba7dc97a2c07f026
rafaelbcastilhos <rafaelbcastilhos@gmail.com>
2021-07-20 12:29:32 -0300

    update app and add integration bots

--- app.py
+++ app.py
@@ -4,12 +4,16 @@ from SistemaChatBot import SistemaChatBot as scb
 from Bots.BotCansado import BotCansado
 from Bots.BotAgiota import BotAgiota
 from Bots.BotSulMatogrossense import BotSulMatogrossense
+from Bots.BotFeliz import BotFeliz
+from Bots.BotTriste import BotTriste
 
 ###construa a lista de bots disponíveis aqui
 lista_bots = [BotCansado("Mai"), 
               BotAgiota("Hulk"), 
               BotSulMatogrossense("Maria"),
-              BotEsqueleto("Tony Dedos")]
+              BotEsqueleto("Tony Dedos"),
+              BotFeliz("Fernandinho Beira Mar"),
+              BotTriste("Rafael")]
 
 sys = scb.SistemaChatBot("Bots Irados", lista_bots)
 sys.inicio()
