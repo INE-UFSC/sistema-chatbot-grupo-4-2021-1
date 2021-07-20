01ab3d5af852243970ba40a5a2723da97fe4fba0
joaomai <joaomai2001@gmail.com>
2021-07-20 12:04:32 -0300

    novo bot, o BotEsqueleto

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 #encoding: utf-8
+from Bots.BotEsqueleto import BotEsqueleto
 from SistemaChatBot import SistemaChatBot as scb
 from Bots.BotCansado import BotCansado
 from Bots.BotAgiota import BotAgiota
 from Bots.BotSulMatogrossense import BotSulMatogrossense
 
 ###construa a lista de bots disponíveis aqui
-lista_bots = [BotCansado("Mai"), BotAgiota("Hulk"), BotSulMatogrossense("Maria")]
+lista_bots = [BotCansado("Mai"), 
+              BotAgiota("Hulk"), 
+              BotSulMatogrossense("Maria"),
+              BotEsqueleto("Tony Dedos")]
 
 sys = scb.SistemaChatBot("Bots Irados", lista_bots)
 sys.inicio()
