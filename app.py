2696ef2162034e6253c3b2448f29655697d228b3
joaomai <joaomai2001@gmail.com>
2021-07-20 11:22:43 -0300

    instanciação dos bots corretos no app

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 #encoding: utf-8
+from Bots.BotAgiota import BotAgiota
 from SistemaChatBot import SistemaChatBot as scb
-from Bots.BotZangado import BotZangado
+from Bots.BotCansado import BotCansado
 
 ###construa a lista de bots disponíveis aqui
-lista_bots = [BotZangado("Yoda")]
+lista_bots = [BotCansado("Mai"), BotAgiota("Hulk")]
 
-sys = scb.SistemaChatBot("CrazyBots",lista_bots)
+sys = scb.SistemaChatBot("Bots Irados", lista_bots)
 sys.inicio()
